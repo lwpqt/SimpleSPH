28e03a2c0c3cb2ae62474aace49e9fac4454a91d
groundexp <meade4@yandex.ru>
2017-02-13 22:01:25 +0300

    changed tabs

--- main.cpp
+++ main.cpp
@@ -33,21 +33,21 @@ struct particles {
 } particles[Particles];
 
 void reshape(int w, int h) {
-        glViewport(0, 0, w, h);
-       	glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        gluOrtho2D(0, w, 0, h);
+		glViewport(0, 0, w, h);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluOrtho2D(0, w, 0, h);
         glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
+		glLoadIdentity();
 }
 
 void display(){	
 		glClear(GL_COLOR_BUFFER_BIT);
 		glBegin(GL_POINTS);
-      	glColor3d(1,0,1);
-        glVertex3d(350, 350, 0); 
+		glColor3d(1,0,1);
+		glVertex3d(350, 350, 0); 
 		glEnd();
-      	glutSwapBuffers();
+		glutSwapBuffers();
 }
 
 void initialise(){
@@ -57,13 +57,13 @@ void initialise(){
 int main (int argc, char * argv[]){
 	srand(time(NULL));
 	initialise();
-        glutInit(&argc, argv);
-        glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
-       	glutInitWindowSize(WIDTH, HIGH);
-        glutCreateWindow("Particles simaulation");
-       	glutReshapeFunc(reshape);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
+	glutInitWindowSize(WIDTH, HIGH);
+	glutCreateWindow("Particles simaulation");
+	glutReshapeFunc(reshape);
 	glutDisplayFunc(display);
-       	glutIdleFunc(display);
-       	glutMainLoop();
-        return 0;
+	glutIdleFunc(display);
+	glutMainLoop();
+	return 0;
 }
